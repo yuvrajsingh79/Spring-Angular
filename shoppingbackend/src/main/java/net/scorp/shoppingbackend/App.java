82ed13b02793a606ad3bbc9f85d8d8ff33bbda11
yuvrajsingh79 <singhyuvraj79@gmail.com>
2018-08-21 00:57:28 +0530

    Project Setup completed

---
+++ shoppingbackend/src/main/java/net/scorp/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.scorp.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
